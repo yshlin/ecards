06a19719523bd5994e971c5b5482f06ea0a12e78
Eddie Lin <yshlin@gmail.com>
2021-06-11 07:22:27 +0800

    Put cards on top-layer.

--- 2021-ngpc-disciple-class/index.js
+++ 2021-ngpc-disciple-class/index.js
@@ -3,6 +3,7 @@ $(() => {
     const bell = new Audio('sound/bell.mp3');
     let $ty = $('#thankyou');
     let $gifts = $('.gift');
+    let $topLayer = $('.top-layer');
     $ty.on('click', e => {
         $(e.target).addClass('clicked');
         $gifts.addClass('clicked');
@@ -20,12 +21,17 @@ $(() => {
             let o2 = $gift.offset();
             $gift.find('.card').attr('style', `top: ${o.top - o2.top + 100}px; left: ${o.left - o2.left + 150}px;`);
         }
+        let giftClass = $gift.attr('class').replace(/.*(gift[0-9]+).*/g, '$1');
+        $topLayer.attr('class', 'gift top-layer show '+giftClass);
         whoosh.play();
     }).on('mouseleave', e => {
         let $gift = $(e.target);
         $gift.removeClass('openl');
         $gift.removeClass('openr');
         $gift.find('.card').attr('style', '');
+        let giftClass = $gift.attr('class').replace(/.*(gift[0-9]+).*/g, '$1');
+        $topLayer.removeClass('show');
+        $topLayer.removeClass(giftClass);
     });
 
 });
